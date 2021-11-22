937a69ee00fde7a4e9d30ca5a73dc2974ab4c13f
albonicc <nikygam@gmail.com>
2021-11-21 22:13:45 -0600

    Modified integral.c and added an excel file with execution results

--- Integral/integral.c
+++ Integral/integral.c
@@ -2,16 +2,17 @@
 #include <omp.h>
 static long num_pasos = 100000000; 
 //static long num_pasos = 500; 
-   
+ 
 //static long num_pasos = 1; 
-double paso;
+double paso, totalTime;
 
 //#define NUM_THREADS 18
 
 //int NUM_THREADS;
 
 void main() {
-	for(int NUM_THREADS = 1; NUM_THREADS<20; NUM_THREADS++){
+	printf("num_threads,execution_time\n");
+	for(int NUM_THREADS = 1; NUM_THREADS<201; NUM_THREADS++) {
 		int i, nthreads; 
 		double pi, sum [NUM_THREADS], t1, t2, tiempo; 
 		paso=1.0/num_pasos; 
@@ -38,9 +39,11 @@ void main() {
 			pi+=sum[i]*paso;
 			//printf("el valor es %lf  con paso %lf  y pi igual a %lf \n",sum[i],paso,pi);
 		}
+
 		const double endTime = omp_get_wtime();
 		tiempo=t2-t1;
+		totalTime = endTime - startTime;
 		//printf("resultado es pi= (%lf), con %ld pasos \n",pi,num_pasos); 
-		printf("Tiempo = %lf\n\n", (endTime - startTime));
+		printf("%d,%lf\n", NUM_THREADS, totalTime);
 	}
 }
