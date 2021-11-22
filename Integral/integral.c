288eb885df7a8e5078b5fb6c373102dcc285ffbc
Ale Oliva <aleoliva9@hotmail.com>
2021-11-21 21:32:13 -0600

    add for loop

--- Integral/integral.c
+++ Integral/integral.c
@@ -6,42 +6,41 @@ static long num_pasos = 100000000;
 //static long num_pasos = 1; 
 double paso;
 
-#define NUM_THREADS 18
+//#define NUM_THREADS 18
 
 //int NUM_THREADS;
 
 void main() {
+	for(int NUM_THREADS = 1; NUM_THREADS<20; NUM_THREADS++){
+		int i, nthreads; 
+		double pi, sum [NUM_THREADS], t1, t2, tiempo; 
+		paso=1.0/num_pasos; 
+		omp_set_num_threads (NUM_THREADS);
+		const double startTime = omp_get_wtime();
 
-	//scanf("%lu", &num_pasos);
-	int i, nthreads; 
-	double pi, sum [NUM_THREADS], t1, t2, tiempo; 
-	paso=1.0/num_pasos; 
-	omp_set_num_threads (NUM_THREADS);
-	const double startTime = omp_get_wtime();
+		#pragma omp parallel
 
-#pragma omp parallel
+		{
+			int i, id, nthrds; 
+			double x;
+			id = omp_get_thread_num(); 
+			nthrds = omp_get_num_threads(); 
+			if (id==0) nthreads=nthrds; 
 
-{
-	int i, id, nthrds; 
-	double x;
-	id = omp_get_thread_num(); 
-	nthrds = omp_get_num_threads(); 
-	if (id==0) nthreads=nthrds; 
+			for (i=id, sum[id] =0.0;i<num_pasos; i=i+nthrds) {
+				x=(i + 0.5) *paso;
+				sum[id]+=4.0/(1.0+x*x);
 
-	for (i=id, sum[id] =0.0;i<num_pasos; i=i+nthrds) {
-		x=(i + 0.5) *paso;
-		sum[id]+=4.0/(1.0+x*x);
+			} 
+		}
 
-	} 
-}
-
-for (i=0,pi=0.0;i<nthreads; i++) {
-	pi+=sum[i]*paso;
-	printf("el valor es %lf  con paso %lf  y pi igual a %lf \n",sum[i],paso,pi);
-}
-	const double endTime = omp_get_wtime();
-	tiempo=t2-t1;
-	printf("resultado es pi= (%lf), con %ld pasos \n",pi,num_pasos); 
-	printf("Tiempo es %lf\n\n", (endTime - startTime));
-
+		for (i=0,pi=0.0;i<nthreads; i++) {
+			pi+=sum[i]*paso;
+			printf("el valor es %lf  con paso %lf  y pi igual a %lf \n",sum[i],paso,pi);
+		}
+			const double endTime = omp_get_wtime();
+			tiempo=t2-t1;
+			printf("resultado es pi= (%lf), con %ld pasos \n",pi,num_pasos); 
+			printf("Tiempo es %lf\n\n", (endTime - startTime));
+	}
 }
